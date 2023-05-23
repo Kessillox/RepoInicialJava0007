2d51b7aa4fac713d5e22aadbefe63e15103eff93
Manpif <manuel.pinot97@gmail.com>
2023-05-23 14:30:02 -0400

    Add: cantidad, nombre y edad asistentes

--- src/nerdsdigital/CapacitacionNerds.java
+++ src/nerdsdigital/CapacitacionNerds.java
@@ -9,20 +9,23 @@ public class CapacitacionNerds {
         int id = 0;
         String rutCliente = "";
         String nombreCliente = "";
-        String direcciónCliente = "";
+        String direccionCliente = "";
         String comunaCliente = "";
         String numeroTelefonoCliente = "";
 
         //Datos informativos capacitacion
-        String día = "";
-        String hora = "";
-        String lugar = "";
-        String duración = "";
-        int cantidadDeAsistentes = 0;
+        String diaCapacitacion = "";
+        String horaCapacitacion = "";
+        String lugarCapacitacion = "";
+        String duracionCapacitacion = "";
+        //int cantidadAsistentes = 0;
 
         // Datos Asistentes
         String nombreAsistente = "";
         int edadAsistente = 0;
+        int menores25 = 0;
+        int entre26y35 = 0;
+        int mayores35 = 0;
 
         //Datos funcionamiento
         int nCapacitaciones = 0;
@@ -38,16 +41,41 @@ public class CapacitacionNerds {
                nombreCliente = JOptionPane.showInputDialog("Ingrese el nombre del cliente");
             }while (nombreCliente.isBlank());
             do {
-               direcciónCliente = JOptionPane.showInputDialog("Ingrese la direccion del cliente");
-            }while (direcciónCliente.isBlank());
+               direccionCliente = JOptionPane.showInputDialog("Ingrese la direccion del cliente");
+            }while (direccionCliente.isBlank());
             do {
                 comunaCliente = JOptionPane.showInputDialog("Ingrese la comuna del cliente");
             }while (comunaCliente.isBlank());
             do {
                 numeroTelefonoCliente = JOptionPane.showInputDialog("Ingrese el telefono del cliente");
             }while(numeroTelefonoCliente.isBlank());
+            do {
+                diaCapacitacion = JOptionPane.showInputDialog("Ingrese el dia de la capacitacion");
+            }while(diaCapacitacion.isBlank());
+            do {
+                horaCapacitacion = JOptionPane.showInputDialog("Ingrese el hora de la capacitacion");
+            }while(horaCapacitacion.isBlank());
+            do {
+                lugarCapacitacion = JOptionPane.showInputDialog("Ingrese el lugar de la capacitacion");
+            }while(lugarCapacitacion.isBlank());
+            do {
+                duracionCapacitacion =  JOptionPane.showInputDialog("Ingrese la duración de la capacitacion en minutos");
+            }while(duracionCapacitacion.isBlank());
 
+        }
+        int cantidadAsistentes = 0;
 
+        cantidadAsistentes = Integer.parseInt(JOptionPane.showInputDialog("cantidad de asistentes")
+        );
+        for (int i = 0; i < cantidadAsistentes; i++) {
+            id +=1;
+
+            do {
+               nombreAsistente = JOptionPane.showInputDialog("Ingrese el nombre del asistente");
+            }while(nombreAsistente.isBlank());
+            do {
+                edadAsistente = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el edad del asistente"));
+            }while (edadAsistente < 18 || edadAsistente > 60);
         }
 
 
