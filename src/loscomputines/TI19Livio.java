f0ce8bd08eb0ae8d0a425825a3fcb20db2fe47ba
Livio <livio.gutierrez.v@gmail.com>
2023-05-24 20:57:52 -0400

    agregando  commentarios

--- src/loscomputines/TI19Livio.java
+++ src/loscomputines/TI19Livio.java
@@ -10,10 +10,11 @@ public class TI19Livio {
         Scanner scaner = new Scanner(System.in);
         String palabra = "";
 
-        //Varaibles del abecedario
-
+        //pidiendo palabra
         System.out.println("Escriba su palabra");
         palabra= scaner.nextLine();
+
+        //verificando que no este vacia
         if (palabra == ""){
             do {
                 System.out.println("Porfavor ingrese una palabra");
@@ -21,30 +22,40 @@ public class TI19Livio {
             }while (palabra == "");
         }
 
-        System.out.println("Palabra: " + palabra);
+        //Conviertiendo las mayusculas en minisculas
         palabra = palabra.toLowerCase();
 
+        //creando el mapa donde pondremos un char y un numero
         Map<Character, Integer> conteo = new HashMap<>();
 
         //Creando  array y almacenando caracter por caracter
         char [] palabraIngresada = new char[palabra.length()];
 
-        //Recorriendo palabra caracter por caracter
+        //creando las letras del abecedario con valor 0
         for (char letra = 'a'; letra <= 'z'; letra++){
             conteo.put(letra,0);
         }
 
+        //Recorriendo palabra caracter por caracter
         for (int in=0; in < palabra.length(); in++ ){
+            //almacenandole a la variable caracteres la palabra y sus caracteres
             char caracteres = palabra.charAt(in);
+            //Imprimiendo por consola la palabra caracter por caracter
             System.out.println(palabra.charAt(in));
 
+            //comprobando que es se alla ingresado un caracter
             if (conteo.containsKey(caracteres)){
+                //si hay un caracter se le suma 1
                 conteo.put(caracteres, conteo.get(caracteres)+1);
             }
         };
-
+        System.out.println();
+        System.out.println("Letras del abecedario repetidas");
+        //creando las letras de labesedario
         for (char letra = 'a'; letra <= 'z'; letra++){
+            //
             int conteoLetra = conteo.getOrDefault(letra, 0);
+            //imprimiendo los resultados de letras con la cantidad de letras repetidas
             System.out.println(letra+": " + conteoLetra );
         }
 
