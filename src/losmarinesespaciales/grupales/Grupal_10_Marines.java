5513c1474aa37484d805c871ec01df038482ad18
Leandro Villalba <leandrovillalba.dev>
2023-05-25 15:35:06 -0400

    agregando promedio de caificaciones

--- src/losmarinesespaciales/grupales/Grupal_10_Marines.java
+++ src/losmarinesespaciales/grupales/Grupal_10_Marines.java
@@ -4,6 +4,7 @@ public class Grupal_10_Marines {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int opcion, dia, cantidadAsistentes, duracion, calificacion, suma;
+        double promedio;
         String hora, lugar, nombre;
 
         do {
@@ -12,7 +13,7 @@ public class Grupal_10_Marines {
             System.out.println("Sistema de Capacitaciones\n");
             System.out.println("Que operacion deseas realizar?\n");
             System.out.println("1. Registrar Capacitacion");
-            System.out.println("4. Salir");
+            System.out.println("2. Salir");
             System.out.print("Selecciona una opción: ");
 
             opcion = scanner.nextInt();
@@ -30,44 +31,43 @@ public class Grupal_10_Marines {
                     duracion = scanner.nextInt();
                     System.out.println("Ingresa la cantidad de asistentes a la capacitacion");
                     cantidadAsistentes = scanner.nextInt();
-                    System.out.println("Datos registrados correctamente!");
-                    System.out.println("Los datos ingresados fueron los siguientes: ");
-                    System.out.println("Dia de capacitacion: " + dia);
-                    System.out.println("Hora de capacitacion: " + hora);
-                    System.out.println("Lugar de capacitacion: " + lugar);
-                    System.out.println("Duracion de capacitacion: " + duracion);
-                    System.out.println("Cantidad de asistentes a la capacitacion: " + cantidadAsistentes);
 
-                    String [][] arreglo = new String[cantidadAsistentes][2];
-
+                    String [][] arreglo = new String[cantidadAsistentes][2]; // Arreglo bidimensional: nombre, calificación
+                    int [] calificaciones = new int[cantidadAsistentes]; // Arreglo de calificaciones
 
                     for(int i=0; i<cantidadAsistentes; i++) {
                         System.out.println("Ingrese su nombre:");
                         arreglo[i][0] = nombre=scanner.next();
 
                         calificacion=0;
-                        suma = 0;
+
                         while(calificacion <1 || calificacion >7) {
 
                             System.out.println("Ingrese su calificación al evento (1 a 7):");
                             calificacion=scanner.nextInt();
                             String calificacionString = Integer.toString(calificacion);
                             arreglo[i][1]=calificacionString;
+                            calificaciones[i] = calificacion;
 
                         }
 
                     }
                     System.out.println("");
+                    /*
+                    * Inicicializo la variable suma en 0
+                    *
+                    * Recorro arreglo de las calificaciones de los asistentes
+                    *
+                    * Calculo promedio*/
 
-                    System.out.println("********* Calificaciones *********");
+                    suma = 0;
                     for (int i=0; i<cantidadAsistentes; i++) {
-                        for (int j=0; j<2; j++) {
 
-                            System.out.print(arreglo[i][j] + " ");
-
-                        }
-                        System.out.println("");
+                        suma = suma + calificaciones[i];
+                        //System.out.println(suma);
                     }
+
+                    promedio = suma / cantidadAsistentes;
 
                     System.out.println("Los datos ingresados fueron los siguientes: ");
                     System.out.println("Dia de capacitacion: " + dia);
@@ -75,6 +75,7 @@ public class Grupal_10_Marines {
                     System.out.println("Lugar de capacitacion: " + lugar);
                     System.out.println("Duracion de capacitacion: " + duracion);
                     System.out.println("Cantidad de asistentes a la capacitacion: " + cantidadAsistentes);
+                    System.out.println("El promedio de las notas asignadas es: " + promedio);
 
 
                     break;
