c20ceb3c41c1470a320ff4824d588adc2282bebc
Manpif <manuel.pinot97@gmail.com>
2023-05-28 14:03:16 -0400

    TI21Manuel terminado

--- src/nerdsdigital/TI21Manuel.java
+++ src/nerdsdigital/TI21Manuel.java
@@ -1,4 +1,38 @@
 package nerdsdigital;
 
 public class TI21Manuel {
+
+/*package Clase11; //Debe iniciar con minúscula.
+public class _EJemplO1 { //No puede empezar con guion bajo, y solo la primera letra en mayúscula a no ser que exista otra palabra.
+    public static void main (String args[]) {
+
+        int $algo = 5; //No puede llevar el signo peso
+        String Var1 = "En un lugar de la Mancha, de cuyo
+        nombre no quiero acordarme, no ha mucho tiempo que vivía un
+        hidalgo de los de lanza en astillero, adarga antigua, rocín flaco
+        y galgo corredor. "
+
+                + "Una olla de algo más vaca que carnero,
+        salpicón las más noches, duelos y quebrantos los sábados,
+        lantejas los viernes, algún palomino de añadidura los domingos,
+                consumían las tres partes de su hacienda.";
+
+                //Este String no se debe concatenar
+
+//El resto della concluían sayo de velarte, calzas de
+        velludo para las fiestas, con sus pantuflos de lo mesmo,
+//y los días de entresemana se honraba con su vellorí de lo más fino
+
+    //Este String debería eliminarse
+
+        int otraCosa;
+        otraCosa = 6;
+        System.out.println("El valor de la primera variable es "
+                + $algo + " y de la segunda es " + otraCosa);
+
+        System.out.println("¿Te cuento un cuento?: " + Var1);
+    }
+ */
+
+
 }
