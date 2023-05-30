d46b5bffe8450dcbd7cd18737fb95bf0fb5a2a02
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-05-29 22:28:00 -0400

    Tercera revision by Luis Zambrano TG-11-Marines 22:27

--- src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
+++ src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
@@ -1,24 +1,26 @@
 package losmarinesespaciales.grupales.grupal_11_marines;
 
-public class Grupal_11_Marines{
+public class Grupal_11_Marines {
     public static void main(String[] args) {
+
         final boolean VALOR_CONSTANTE = false;
 
-        String mensaje = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis est ac ante viverra, vel efficitur leo consequat." +
-                " Maecenas quis lorem sit amet diam consequat lacinia non nec lacus." +
-                " Phasellus egestas quam non dui dictum, sed fermentum dolor efficitur." +
-                " Aliquam volutpat ex sodales pulvinar scelerisque. Donec sodales cursus tortor eu aliquam." +
-                " Curabitur id purus arcu." +
-                " Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;" +
-                " Suspendisse pretium, quam non consectetur scelerisque, diam tortor iaculis arcu, nec tincidunt diam eros sed urna." +
-                " Ut at consequat enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
-                " Donec sit amet ultricies dui. Donec aliquet rhoncus velit et sollicitudin.";
+        String mensaje = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis est ac" +
+                         "ante viverra, vel efficitur leo consequat. Maecenas quis lorem sit amet diam con" +
+                         "sequat lacinia non nec lacus. Phasellus egestas quam non dui dictum, sed ferment" +
+                         "um dolor efficitur. Aliquam volutpat ex sodales pulvinar scelerisque. Donec soda" +
+                         "les cursus tortor eu aliquam. Curabitur id purus arcu. Vestibulum ante ipsum" +
+                         "primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse" +
+                         "pretium, quam non consectetur scelerisque, diam tortor iaculis arcu, nec" +
+                         "tincidunt diam eros sed urna. Ut at consequat enim. Orci varius natoque" +
+                         "penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
+                         "Donec sit amet ultricies dui. Donec aliquet rhoncus velit et sollicitudin.";
 
         int repetirHasta = 10;
 
-        for (int i=0; i < repetirHasta; i++) {
+        for (int i = 0; i < repetirHasta; i++) {
 
-            System.out.println("Valor: " + (i*i));
+            System.out.println("Valor: " + (i * i));
         }
     }
 }
