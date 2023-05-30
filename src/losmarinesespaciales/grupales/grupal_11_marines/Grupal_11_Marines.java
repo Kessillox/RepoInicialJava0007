25dd4f5a894ff3844c825616e394196a2fa465b8
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-05-29 22:14:51 -0400

    Segunda revision by Luis Zambrano 29-05-2023 22:14

--- src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
+++ src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
@@ -2,7 +2,7 @@ package losmarinesespaciales.grupales.grupal_11_marines;
 
 public class Grupal_11_Marines{
     public static void main(String[] args) {
-        final boolean valorConstante = false;
+        final boolean VALOR_CONSTANTE = false;
 
         String mensaje = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis est ac ante viverra, vel efficitur leo consequat." +
                 " Maecenas quis lorem sit amet diam consequat lacinia non nec lacus." +
@@ -14,9 +14,9 @@ public class Grupal_11_Marines{
                 " Ut at consequat enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
                 " Donec sit amet ultricies dui. Donec aliquet rhoncus velit et sollicitudin.";
 
-        int hola = 10;
+        int repetirHasta = 10;
 
-        for (int i=0; i < hola; i++) {
+        for (int i=0; i < repetirHasta; i++) {
 
             System.out.println("Valor: " + (i*i));
         }
@@ -30,15 +30,15 @@ public class Grupal_11_Marines{
 package Clase11; <-- El package siempre es minusculas y aca esta en Capitalize Format y tiene el nombre Clase cuando es un package (Ademas debe tener el nombre del paquete no de clase)
 public class $EjeMpl0_2 { <-- El nombre de la clase debe ser en Capitalize Format (La priemra letra de la palabra en mayusculas) y no tener $ ni mayusculas entre letras.
 public static void main(String[] args) {
-final boolean valorconstante = false; <-- El nombre valorconstante esta mal, debe ser en CamelCase, por lo que seria valorConstante
+final boolean valorconstante = false; <-- El nombre valorconstante esta mal, debe ser en mayusculas, por lo que seria VALOR_CONSTANTE
 String mensaje = "Lorem ipsum dolor sit amet,...... <-- El mensaje se separo en diferentes concatenaciones para hacerlo mas legible ya que estaba ordenado por defecto de forma
                                                   erronea, ni siquiera formaba un String
 
 
 int Ola; <-- Si bien una variable puede llevar cualquier nombre, por convencion se indica que debe ser una palabra existente y descriptiva de lo que se va a operar, sin embargo estaria bien
-             Por lo menos escribir hola como corresponde. Ademas de eso la variable se inicializo en la misma linea, considero que es mas correcto asi.
-Ola = 10;
-for (int i=0;i<Ola;i++) { <-- El for en si esta bien sintacticamente, el problema radica en que es dificil de leer al estar todos los caracteres de la variable i sin espaciado
+             Por lo menos escribir hola como corresponde. Ademas de eso la variable se inicializo en la misma linea, considero que se ajusta mas asi a los estandares.
+Ola = 10; Se cambio el nombre de la variable de Ola a repetirHasta que es mas descriptivo e indica hasta donde debe repetir el for
+for (int i=0;i<Ola;i++) { <-- El for en si esta bien sintacticamente, el problema radica en que es dificil de leer al estar todos los caracteres de la variable i sin espaciado entre expresioens y operadores
 
 System.out.println("Valor: " + (i*i));
 }
