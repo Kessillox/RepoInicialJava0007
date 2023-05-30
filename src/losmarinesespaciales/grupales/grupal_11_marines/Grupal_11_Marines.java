d55907c5da4f6805bca4083245dccbe3cd4de111
StudioMontt <electri.mv@gmail.com>
2023-05-29 23:48:18 -0400

    Resubiendo mis cambios

--- src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
+++ src/losmarinesespaciales/grupales/grupal_11_marines/Grupal_11_Marines.java
@@ -2,26 +2,35 @@ package losmarinesespaciales.grupales.grupal_11_marines;
 
 public class Grupal_11_Marines {
     public static void main(String[] args) {
-
         final boolean VALOR_CONSTANTE = false;
 
-        String mensaje = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis est ac" +
-                         "ante viverra, vel efficitur leo consequat. Maecenas quis lorem sit amet diam con" +
-                         "sequat lacinia non nec lacus. Phasellus egestas quam non dui dictum, sed ferment" +
-                         "um dolor efficitur. Aliquam volutpat ex sodales pulvinar scelerisque. Donec soda" +
-                         "les cursus tortor eu aliquam. Curabitur id purus arcu. Vestibulum ante ipsum" +
-                         "primis in faucibus orci luctus et ultrices posuere cubilia curae; Suspendisse" +
-                         "pretium, quam non consectetur scelerisque, diam tortor iaculis arcu, nec" +
-                         "tincidunt diam eros sed urna. Ut at consequat enim. Orci varius natoque" +
-                         "penatibus et magnis dis parturient montes, nascetur ridiculus mus." +
-                         "Donec sit amet ultricies dui. Donec aliquet rhoncus velit et sollicitudin.";
+        String mensaje = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec facilisis est ac ante viverra," +
+                " vel efficitur leo consequat." +
+                " Maecenas quis lorem sit amet diam consequat lacinia non nec lacus." +
+                " Phasellus egestas quam non dui dictum, sed fermentum dolor efficitur." +
+                " Aliquam volutpat ex sodales pulvinar scelerisque. Donec sodales cursus tortor eu aliquam." +
+                " Curabitur id purus arcu." +
+                " Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;" +
+                " Suspendisse pretium, quam non consectetur scelerisque, diam tortor iaculis arcu, nec tincidunt diam" +
+                " eros sed urna." +
+                " Ut at consequat enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur" +
+                " ridiculus mus." +
+                " Donec sit amet ultricies dui. Donec aliquet rhoncus velit et sollicitudin.";
 
-        int repetirHasta = 10;
+        int hola = 10;
 
-        for (int i = 0; i < repetirHasta; i++) {
+        for (int i = 0; i < hola; i++) {
 
             System.out.println("Valor: " + (i * i));
         }
+        /*
+        Esto es un comentario
+        demasiado grande
+        para que esté en una
+        sola línea
+        */
+        System.out.println("El mensaje es" + mensaje);
+        System.out.println(" El valor de la constante es: " + VALOR_CONSTANTE);
     }
 }
 
@@ -29,30 +38,37 @@ public class Grupal_11_Marines {
 
 ############## CODIGO ORIGINAL CON ERRORES ##########################
 
-package Clase11; <-- El package siempre es minusculas y aca esta en Capitalize Format y tiene el nombre Clase cuando es un package (Ademas debe tener el nombre del paquete no de clase)
-public class $EjeMpl0_2 { <-- El nombre de la clase debe ser en Capitalize Format (La priemra letra de la palabra en mayusculas) y no tener $ ni mayusculas entre letras.
+package Clase11; <-- El package siempre es minúscula y aca está en Capitalize Format y tiene el nombre Clase cuando es
+        un package (Además debe tener el nombre del paquete no de clase)
+public class $EjeMpl0_2 { <-- El nombre de la clase debe ser en Capitalize Format (La primera letra de la palabra en
+        mayúsculas) o PascalCase, y no tener caracteres como la "$", ni mayúsculas entre letras.
 public static void main(String[] args) {
-final boolean valorconstante = false; <-- El nombre valorconstante esta mal, debe ser en mayusculas, por lo que seria VALOR_CONSTANTE
-String mensaje = "Lorem ipsum dolor sit amet,...... <-- El mensaje se separo en diferentes concatenaciones para hacerlo mas legible ya que estaba ordenado por defecto de forma
-                                                  erronea, ni siquiera formaba un String
 
+final boolean valorconstante = false; <-- El nombre valorconstante está mal, al declararse con "final" se asume que es
+        una constante, por lo que debe ir en MAYUSCULAS_Y_SON_SEPARACION.
+String mensaje = "Lorem ipsum dolor sit amet,...... <-- El mensaje se separó en diferentes concatenaciones para hacerlo
+        más legible (máximo 80 caracteres), ya que estaba escrito en forma lineal.
 
-int Ola; <-- Si bien una variable puede llevar cualquier nombre, por convencion se indica que debe ser una palabra existente y descriptiva de lo que se va a operar, sin embargo estaria bien
-             Por lo menos escribir hola como corresponde. Ademas de eso la variable se inicializo en la misma linea, considero que se ajusta mas asi a los estandares.
-Ola = 10; Se cambio el nombre de la variable de Ola a repetirHasta que es mas descriptivo e indica hasta donde debe repetir el for
-for (int i=0;i<Ola;i++) { <-- El for en si esta bien sintacticamente, el problema radica en que es dificil de leer al estar todos los caracteres de la variable i sin espaciado entre expresioens y operadores
+int Ola; <-- Si bien una variable puede llevar cualquier nombre en camelCase, además debe ser una palabra existente y
+        descriptiva de lo que se va a operar. Además, la indentación debe ser igual a la fila superior si son del mismo
+        nivel.
+Ola = 10; <-- Por otro lado, la variable se inicializó en la misma línea anterior, considerando que es más correcto asi.
 
-System.out.println("Valor: " + (i*i));
+for (int i=0;i<Ola;i++) { <-- El for en si esta bien sintácticamente, el problema radica en que es difícil de leer al
+        estar todos los caracteres de la variable i sin espaciado.
+
+System.out.println("Valor: " + (i*i)); <-- Al igual que la fila anterior, se separa los operadores binarios de sus
+        operandos para una mejor legibilidad.
 }
 
-############################# FIN LUIS ########################################################################
-//Esto es un comentario
+//Esto es un comentario  <-- Aquí es necesario utilizar un comentario en bloque, y no uno por cada línea.
 //demasiado grande
 //para que esté en una
 //sola línea
-System.out.println( "El mensaje " + " es" + mensaje);
-System.out.println(" El valor de la constante es: " +
-valorconstante);
+
+System.out.println( "El mensaje " + " es" + mensaje); <-- Se elimina el espacio delante del string, y se unen las cadenas
+        para tener un código más limpio.
+System.out.println(" El valor de la constante es: " + valorconstante); <-- Aquí fue sólo necesario cambiar VALOR_CONSTANTE
 }
 }
 
