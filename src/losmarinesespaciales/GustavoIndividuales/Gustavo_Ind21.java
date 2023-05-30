c67af2e294c2f4ecc0925c75c13307d47857c890
StudioMontt <electri.mv@gmail.com>
2023-05-30 17:36:17 -0400

    subiendo rama y codigo TI-21-GustavoV

---
+++ src/losmarinesespaciales/GustavoIndividuales/Gustavo_Ind21.java
@@ -0,0 +1,24 @@
+package losmarinesespaciales.gustavoindividuales;
+
+public class Gustavo_Ind21 {
+    //package clase11;
+    //public class EjemploO1 {
+    public static void main (String[] args) {
+
+        int algo = 5;
+        String Var1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un" +
+            " hidalgo de los de lanza en astillero, adarga antigua, rocín flaco y galgo corredor. " +
+            " Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, " +
+            " lentejas los viernes, algún palomino de añadidura los domingos, consumían las tres partes de su hacienda.";
+
+    /*
+    El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de lo mesmo,
+    y los días de entresemana se honraba con su vellorí de lo más fino
+    */
+
+        int otraCosa = 6;
+        System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa);
+
+        System.out.println("¿Te cuento un cuento?: " + Var1);
+    }
+}
