804e3b42ffbfe2671d989e15a1540c43d425f2fd
nataliaperezfierro <natalia.perez.fierro@gmail.com>
2023-05-30 12:57:19 -0400

    clase usuario atributos metodos

---
+++ src/duality/NataliaPerez.java
@@ -0,0 +1,6 @@
+package duality;
+
+public class NataliaPerez {
+}
+// INDIVIDUAL 15
+
--- src/duality/tg13/Usuario.java
+++ src/duality/tg13/Usuario.java
@@ -1,4 +1,51 @@
 package duality.tg13;
 
 public class Usuario {
+    private String rut;
+    private String nombre;
+    private String fechaNac;
+
+    public Usuario(){}
+
+    public Usuario(String rut, String nombre, String fechaNac){
+        this.rut=rut;
+        this.nombre=nombre;
+        this.fechaNac=fechaNac;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "rut='" + rut + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", fechaNac='" + fechaNac + '\'' +
+                '}';
+    }
 }
+
+
+
