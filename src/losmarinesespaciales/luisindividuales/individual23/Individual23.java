a52b75eb077ca45c01c214c7f40ff6be186af9ec
Luis Zambrano <LuisZambrano.Work@gmail.com>
2023-05-31 06:16:30 -0400

    Hola Mundo

--- src/losmarinesespaciales/luisindividuales/individual23/Individual23.java
+++ src/losmarinesespaciales/luisindividuales/individual23/Individual23.java
@@ -1,4 +1,7 @@
 package losmarinesespaciales.luisindividuales.individual23;
 
 public class Individual23 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
 }
