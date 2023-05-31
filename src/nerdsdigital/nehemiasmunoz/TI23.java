50030d33c0d72e1a662fe215528f1f4318ea71d1
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-05-31 11:57:41 -0400

    Ejercicio 23 resuelto

---
+++ src/nerdsdigital/nehemiasmunoz/TI23.java
@@ -0,0 +1,25 @@
+package nerdsdigital.nehemiasmunoz;
+
+import javax.swing.JOptionPane;
+
+public class TI23 {
+
+    public static void main(String[] args) throws Exception {
+        String cadena;
+        String nuevaCadena = "";
+        String[] cadenaDividida;
+        cadena = JOptionPane.showInputDialog("Ingrese la cadena de texto");
+        cadenaDividida = cadena.split("");
+        for (int i = 0; i < cadenaDividida.length; i++) {
+            if (cadenaDividida[i].toUpperCase() == cadenaDividida[i]) {
+                cadenaDividida[i] = cadenaDividida[i].toLowerCase();
+            } else {
+                cadenaDividida[i] = cadenaDividida[i].toUpperCase();
+            }
+            nuevaCadena += cadenaDividida[i];
+        }
+        System.out.println("Cadena Inicial: " + cadena + "\n Cadena Formateada: " + nuevaCadena);
+    }
+
+    // Nehemias Muñoz
+}
