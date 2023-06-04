2c7745aabcd945375be73151f7db1c7276a04a35
Jucame <jcmerino@uc.cl>
2023-06-04 01:27:24 -0400

    TI_21 corregido agregado

--- src/lossincodigo/juanmerino/TI_21_JM.java
+++ src/lossincodigo/juanmerino/TI_21_JM.java
@@ -3,6 +3,24 @@ package lossincodigo.juanmerino;
 public class TI_21_JM {
 
     public static void main(String[] args) {
-        // c
+
+        int algo = 5;
+        String var1 = "En un lugar de la Mancha, de cuyo nombre no quiero acordarme, " +
+                "no ha mucho tiempo que vivía un hidalgo de los de lanza en astillero, " +
+                "adarga antigua, rocín flaco y galgo corredor. " +
+                "Una olla de algo más vaca que carnero, salpicón las más noches, duelos y quebrantos los sábados, " +
+                "lantejas los viernes, algún palomino de añadidura los domingos, " +
+                "consumían las tres partes de su hacienda.";
+
+        /*
+        El resto della concluían sayo de velarte, calzas de velludo para las fiestas, con sus pantuflos de los mesmo,
+        y los días de entresemana se honraba con su vellorí de lo más fino
+         */
+
+        int otraCosa;
+        otraCosa = 6;
+
+        System.out.println("El valor de la primera variable es " + algo + " y de la segunda es " + otraCosa);
+        System.out.println("¿Te cuento un cuento?: " + var1);
     }
 }
