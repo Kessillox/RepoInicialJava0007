7190a2d486ed96ef3db37fc88bd07f9609b64883
Livio <livio.gutierrez.v@gmail.com>
2023-06-05 11:05:40 -0400

    trabajo 10

--- src/loscomputines/grupal12m4/grupal10/grupal10computines.java
+++ src/loscomputines/grupal12m4/grupal10/grupal10computines.java
@@ -1,94 +1,67 @@
 package loscomputines.grupal12m4.grupal10;
 /**
- * en esta clase tuvimos que correguir algunos errores y malas practicas
+ * en esta clase se utiliza para almacenar una capasita con dia, hora, lugar, duracion, nombre y nota
  * @version 01-06-2023
  * @author Sergio, Livio, Geneis, Nazaly
- * @var ola, i, mensaje, constante
+ * @var cantidadAsistentes, dia, nota, hora, lugar, duracion, i, scanner
  * **/
 import java.util.Scanner;
 public class grupal10computines {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //Variables
-        int dia = 0, cantidadAsistente=0, nota=0, contador=0;
-        String lugar = null, hora = null, duracion=null;
 
-        System.out.println("Ingrese cantidad de asistentes en la capasitacion, por favor");
-        //Pidiendo el numero de asistente
-        cantidadAsistente=scanner.nextInt();
-        do {
+        // Variables
+        int cantidadAsistentes = 0,dia = 0, nota;
+        String hora = null, lugar=null, duracion=null;
 
-            contador++;
-            System.out.println("Numero de Asistente: "+contador);
+        System.out.println("Ingrese la cantidad de asistentes en la capacitación, por favor:");
+        cantidadAsistentes = scanner.nextInt();
+
+        String[][] matriz = new String[cantidadAsistentes][2];
+
+        for (int i = 0; i < cantidadAsistentes; i++) {
+            System.out.println("Asistente " + (i + 1));
+
+            // Ingresar datos de la capacitación
             do {
-                System.out.println("Ingrese dia de capasitacion por favor ");
+                System.out.println("Ingrese el día de la capacitación, por favor:");
                 dia = scanner.nextInt();
-            }while (dia <= 0);
-            scanner.nextLine();
+            } while (dia <= 0);
+
+            scanner.nextLine(); // Limpia el buffer
+
+            System.out.println("Ingrese la Hora de la capacitación, por favor:");
+            hora = scanner.nextLine();
+
+            System.out.println("Ingrese el lugar de la capacitación, por favor:");
+            lugar = scanner.nextLine();
+
+            System.out.println("Ingrese la duración de la capacitación, por favor:");
+            duracion = scanner.nextLine();
+
+            // Ingresar datos del asistente y su calificación
+            System.out.print("Ingrese el nombre del asistente: ");
+            matriz[i][0] = scanner.nextLine();
 
             do {
-                System.out.println("Ingrese la Hora de la capacitacion, por favor");
-                hora = scanner.next();
-            }while (hora == "");
-            scanner.nextLine();
+                System.out.println("Ingrese la calificación del asistente (1-7):");
+                System.out.println("Para ingresar una nota válida, use los valores enteros del 1 al 7.");
+                nota = scanner.nextInt();
+            } while (nota < 1 || nota > 7);
 
-            do {
-                System.out.println("Ingrese el lugar de la capacitacion, por favor");
-                lugar=scanner.next();
-            }while (lugar == "");
-            scanner.nextLine();
+            scanner.nextLine(); // Limpia el buffer
 
-            do {
-                System.out.println("Ingrese la duracion de la capasitacion, por favor");
-                duracion=scanner.next();
-            }while (duracion == "");
-            //Pidiendo datos
+            matriz[i][1] = String.valueOf(nota);
+            System.out.println(); // Salto de línea
 
-            do {
-                //Creando Array para almacenar los nombres de los asistentes
-                String [][] matriz = new String[cantidadAsistente][2];
-                scanner.nextLine();//limpia el scanner
-
-                //Recorrriendo el Array
-                for (int i=0; i<cantidadAsistente; i++){
-
-                    for (int j=0; j<2; j++){
-
-                        if (j==0){
-                            System.out.print("Ingrese su nombre: ");
-                            matriz[i][j]= scanner.next();
-                        }
-                        if (j==1){
-                            do {
-                                System.out.print("Ingrese su calificaion: ");
-                                System.out.println("Para un bueno resultado escriba su nota asi:" + "10,20,30,40,50,60,70" + ", asi no tendra errores en su promedio");
-                                nota = Integer.parseInt(matriz[i][j]= scanner.next());
-
-                                if (nota<=9 || nota>70){
-                                    System.out.println("Numero ingresado incorrecto");
-                                }
-                            }while (nota<=9|| nota>70);
-
-                        }
-                    }
-                }
-
-                for (int i=0; i<cantidadAsistente; i++){
-                    System.out.println("Dia: " + dia);
-                    System.out.println("Hora: " + hora);
-                    System.out.println("Lugar: " + lugar);
-                    System.out.println("Duracion: " + duracion);
-                    for (int j=0; j<2; j++){
-                        System.out.print(matriz[i][j] + " " );
-                        System.out.println("");
-                    }
-
-                }
-
-            }while (cantidadAsistente<=0);
-
-        }while (contador < cantidadAsistente);
-
+            // Mostrar datos de la capacitación y los asistentes
+            System.out.println("Dia: " + dia);
+            System.out.println("Hora: " + hora);
+            System.out.println("Lugar: " + lugar);
+            System.out.println("Duracion: " + duracion);
+            System.out.println("Nombre: " + matriz[i][0]);
+            System.out.println("Calificación: " + matriz[i][1]);
+            System.out.println(); // Salto de línea
+        }
     }
-
 }
